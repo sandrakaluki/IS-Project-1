40493e788f9bea1ffc2377b81016d8c71f10e047
sandrakaluki <130549134+sandrakaluki@users.noreply.github.com>
2024-07-09 14:36:46 +0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,117 @@
+<?php
+?>
+<DOCTYPE html> 
+<html lang="en">
+
+<head>
+<meta charset="utf-8">
+<meta name= "viewport" content="width=device-width">
+<meta name= "keywords" content="carbon footprint,greenhouse gases">
+<meta name="author" content=" Sandra and Adrian"> 
+<title> Carbon Coin - Login </title>
+
+<link rel="stylesheet" href="Frontend/css/style.css">
+<img src="Frontend/images/Environmental Conservation1 .jpg "/>
+</head>
+<section>
+<video src ="Frontend/WhatsApp Video 2024-07-09 at 1.49.36 PM.mp4" muted loop autoplay> </video> 
+<div class = "overlay">
+</section>
+
+<body>
+    <div class="login-container">
+
+ <h1>Carbon Coin
+    <style>
+
+ h1 {
+    color:greenyellow;
+    font-size: 3em;
+    margin-bottom: 20px;
+
+}
+</style>
+
+ </h1>
+
+ <p><img src="C:\Users\User\Documents\strathmore-uni\IS-Project-1\Frontend"></p>
+ <form action="home.php" method="post">
+
+<div class="input-container">
+
+<style>
+    .login-container{
+    background: #00ff73;
+    padding: 40px;
+    border-radius: 10px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    text-align: center;
+    width: 400px;
+    }
+    </style>
+<label for="cname"> Name </label>
+<input required type="text" id="cname" name="cname">
+
+<label for="password"> Password </label>
+<input required type="text" id="password" password="password">
+</div>
+
+<div class="button-container">
+<button type="Enter">Enter
+    <style>
+.button-container {
+    display: flex;
+    justify-content: space-between;
+}
+
+.button-container .btn {
+    padding: 10px 20px;
+    border: none;
+    border-radius: 5px;
+    background-color: #00ff84;
+    color: #ffffff;
+    cursor: pointer;
+    font-size: 1em;
+}
+
+.button-container .btn:hover {
+    background-color: #00b506;
+}
+
+.button-container .btn:active {
+    background-color: #007f2a;
+}
+</style>
+</button>
+<button type="submit" class="btn" onclick="window.location.href='signup.php' ">Create An Account
+    <style>
+.button-container {
+    display: flex;
+    justify-content: space-between;
+}
+
+.button-container .btn {
+    padding: 10px 20px;
+    border: none;
+    border-radius: 5px;
+    background-color: #00ff84;
+    color: #ffffff;
+    cursor: pointer;
+    font-size: 1em;
+}
+
+.button-container .btn:hover {
+    background-color: #00b506;
+}
+
+.button-container .btn:active {
+    background-color: #007f2a;
+}
+</style>
+</button>
+</div>
+</div>
+ </form>
+    </div>
+</body> 
+</html>
